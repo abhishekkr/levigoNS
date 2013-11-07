343c5899dd47ddd20cff297ad5b67be330ceaf13
AbhishekKr <abhikumar163@gmail.com>
2013-11-07 10:31:09 +0530

    example updated with ReadNSRecursive

--- zxtra/check_namespaced_one_day_seconds.go
+++ zxtra/check_namespaced_one_day_seconds.go
@@ -8,7 +8,7 @@ import (
 
   "github.com/jmhodges/levigo"
 
-  lns "github.com/abhishekkr/levigoNS"
+  abklevigoNS "github.com/abhishekkr/levigoNS"
   abkleveldb "github.com/abhishekkr/levigoNS/leveldb"
 )
 
@@ -20,13 +20,14 @@ func EverySecondOfHour(hour int, check string, db *levigo.DB) {
   for sec := 0; sec < 3600; sec++{
     nukey := fmt.Sprintf("127.0.0.1:%s:2013:10:26:%d:%d",
                         check, hour, sec)
+    if sec%500 != 0 { continue }
     val := "up"
     if sec%1000 == 0 {
       val = "down"
     }
-    lns.PushNS(nukey, val, db)
+    abklevigoNS.PushNS(nukey, val, db)
   }
-  fmt.Printf("Hour %s done. Enter 'yes' anytime to end Push action.", hour)
+  fmt.Printf("Hour %d done. Enter 'yes' anytime to end Push action.\n", hour)
 }
 
 func WriteMap(db *levigo.DB){
@@ -48,10 +49,16 @@ func WriteMap(db *levigo.DB){
 func ReadMap(key string, db *levigo.DB){
   var hmap map[string]string
   hmap = make(map[string]string)
-  hmap = lns.ReadNS(key, db)
+  hmap = abklevigoNS.ReadNS(key, db)
   fmt.Println("Total Child Keys found:", len(hmap))
   for k,v := range hmap {
     fmt.Printf("%s => %s\n", k, v)
+  }
+}
+
+func PrintMapRecursive(m abklevigoNS.HashMap){
+  for k,v := range m {
+    fmt.Println("val for key:", k, v)
   }
 }
 
@@ -63,9 +70,16 @@ func main(){
   create_start_time := time.Now()
   db = abkleveldb.CreateDB(*dbpath)
   WriteMap(db)
-  read_start_time := time.Now()
+  fmt.Println("Writing is over.")
   ReadMap("127.0.0.1:status:2013:10:26:12", db)
+  result := abklevigoNS.ReadNSRecursive("127.0.0.1:status", db)
+  read_start_time := time.Now()
+  PrintMapRecursive( result )
+  ReadMap("127.0.0.1:status:2013:10:26", db)
+  fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
   fmt.Printf("\n\nStatistics:\n\tStarted at: %q\n", start_time)
   fmt.Printf("\tCreating DB: %q\n", create_start_time)
   fmt.Printf("\tReading DB: %q\n\tRead For an Hour: %q\n", read_start_time, time.Now())
+  fmt.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  fmt.Println(len(result))
 }
